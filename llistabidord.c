7432ab328db2389cf211836367c61bd24fe071e1
Marc Céspedes <marc.cespedes@students.salle.url.edu>
2018-10-07 19:55:52 +0200

    style: Addapted to guia d'estils La Salle
    Addapted with all the characteristics of "Gui d'estil la Salle"

--- llistabidord.c
+++ llistabidord.c
@@ -2,106 +2,118 @@
 #include "llistabidord.h"
 
 //Creem la llista Bidireccional Ordenada
-LlistaBID LLISTABIDORD_crea(){
-  LlistaBID l;
-  l.cap = (Node*) malloc (sizeof(Node));
-  if(l.cap == NULL){
-    printf("Error!\n");
-  }else{
-    l.ult = (Node*) malloc(sizeof(Node));
-    if (l.ult==NULL){
-      printf("Error\n");
-      free(l.cap);
-    }else{
-      l.pdi = l.ult;
-      l.cap->seg = l.ult;
-      l.ult->ant = l.cap;
-      l.cap->ant = NULL;
-      l.ult->seg = NULL;
-    }
-  }
-  return l;
+LlistaBID LLISTABIDORD_crea() {
+	LlistaBID l;
+	
+	l.cap = (Node*) malloc (sizeof(Node));
+	if (l.cap == NULL) {
+		printf("Error!\n");
+	}
+	else {  
+		l.ult = (Node*) malloc(sizeof(Node));
+		if (l.ult==NULL) {
+			printf("Error\n");
+			free(l.cap);
+		}
+		else {
+			l.pdi = l.ult;
+			l.cap->seg = l.ult;
+			l.ult->ant = l.cap;
+			l.cap->ant = NULL;
+			l.ult->seg = NULL;
+		}
+	}
+	return l;
 }
 
 // Tira endarrera una posició
-void LLISTABIDORD_retrocedeix (LlistaBID *l){
-  if(l->pdi == l->cap){
-    printf("No podem anar mes enrere\n");
-  }else{
-    l->pdi = l->pdi->ant;
-  }
+void LLISTABIDORD_retrocedeix (LlistaBID *l) {
+	if (l->pdi == l->cap) {
+		printf("No podem anar mes enrere\n");
+	}
+	else {
+		l->pdi = l->pdi->ant;
+	}
 }
 
-int LLISTABIDORD_consulta(LlistaBID l){
-  int num;
-  num = -1;
-  if(l.pdi != l.cap && l.pdi != l.ult){
-    num=l.pdi->num;
-  }
+int LLISTABIDORD_consulta(LlistaBID l) {
+	int num;
+	num = -1;
+	
+	if(l.pdi != l.cap && l.pdi != l.ult) {
+		num=l.pdi->num;
+	}
   return num;
 }
 
-void  LLISTABIDORD_esborra(LlistaBID *l){
-  Node *aux;
-  if(l->pdi == l->cap || l->pdi == l->ult){
-    printf("Error\n");
-  }else{
-    aux = l->pdi;
-    aux->ant->seg = aux->seg;
-    aux->seg->ant = aux->ant;
-    l->pdi = l->pdi->seg;
-    free(aux);
-  }
-}
-
-void LLISTABIDORD_destrueix(LlistaBID *l){
-  Node *aux;
-  while (l->cap != NULL) {
-    aux = l->cap;
-    l->cap = l->cap->seg;
-    free(aux);
-  }
-  l->ult = NULL;
-  l->pdi = NULL;
-}
-
-void LLISTABIDORD_vesinici(LlistaBID *l){
-  l->pdi = l->cap->seg;
-}
-
-int LLISTABIDORD_inici(LlistaBID l){
-  return (l.pdi == l.cap);
-}
-
-int LLISTABIDORD_buida(LlistaBID l){
-  return l.cap->seg == l.ult;
-}
-
-void LLISTABIDORD_vesfinal(LlistaBID *l){
-  l->pdi = l->ult->ant;
-}
-
-int LLISTABIDORD_fi(LlistaBID l){
-  return l.pdi == l.ult;
-}
-
-void LLISTABIDORD_avanca(LlistaBID *l){
-  if(l->pdi->seg == NULL){
-    printf("No es pot avançar\n");
-  }else{
-    l->pdi = l->pdi->seg;
-  }
-}
-
-void LLISTABIDORD_insereixdavant(LlistaBID *l, int num){
+void  LLISTABIDORD_esborra(LlistaBID *l) {
 	Node *aux;
-	if(l->pdi == l->cap){
+	
+	if (l->pdi == l->cap || l->pdi == l->ult) {
+		printf("Error\n");
+	}
+	else {
+		aux = l->pdi;
+		aux->ant->seg = aux->seg;
+		aux->seg->ant = aux->ant;
+		l->pdi = l->pdi->seg;
+		free(aux);
+	}
+}
+
+void LLISTABIDORD_destrueix(LlistaBID *l) {
+	Node *aux;
+  
+	while (l->cap != NULL) {
+		aux = l->cap;
+		l->cap = l->cap->seg;
+		free(aux);
+	}
+	l->ult = NULL;
+	l->pdi = NULL;
+}
+
+void LLISTABIDORD_vesInici(LlistaBID *l) {
+	l->pdi = l->cap->seg;
+}
+
+int LLISTABIDORD_inici(LlistaBID l) {
+	return (l.pdi == l.cap);
+}
+
+int LLISTABIDORD_buida(LlistaBID l) {
+	return l.cap->seg == l.ult;
+}
+
+void LLISTABIDORD_vesFinal(LlistaBID *l) {
+	l->pdi = l->ult->ant;
+}
+
+int LLISTABIDORD_fi(LlistaBID l) {
+	return l.pdi == l.ult;
+}
+
+void LLISTABIDORD_avanca(LlistaBID *l) {
+	if (l->pdi->seg == NULL) {
+		printf("No es pot avançar\n");
+	}
+	else {
+		l->pdi = l->pdi->seg;
+	}
+}
+
+void LLISTABIDORD_insereixDavant(LlistaBID *l, int num) {
+	Node *aux;
+	
+	if (l->pdi == l->cap) {
     	printf("Error\n");
-  	}else{
+  	}
+	else {
     	aux =(Node*) malloc (sizeof(Node));
    		if (aux == NULL) {
      	 	printf("Error\n");
-    	}else{
+    	}
+		else {
      	aux->num = num;
      	aux->seg = l->pdi;
     	aux->ant = l->pdi->ant;
@@ -111,34 +123,38 @@ void LLISTABIDORD_insereixdavant(LlistaBID *l, int num){
 	}
 }
 
-void LLISTABIDORD_insereixdarrera(LlistaBID *l, int num){
-  Node *aux;
-  if(l->pdi == l->ult){
-    printf("Error\n");
-  }else{
-    aux =(Node*) malloc (sizeof(Node));
-    if (aux == NULL) {
-      printf("Error\n");
-    }else{
-      aux->num = num;
-      if (l->pdi->seg == NULL) {
-        printf("Error\n");
-      }else{
-        aux->num = num;
-        aux->seg = l->pdi->seg;
-        aux->ant = l->pdi;
-        l->pdi->seg->ant = aux;
-        l->pdi->seg = aux;
-      }
-    }
-  }
+void LLISTABIDORD_insereixDarrera(LlistaBID *l, int num) {
+	Node *aux;
+  
+	if (l->pdi == l->ult) {
+		printf("Error\n");
+	}
+	else {
+		aux =(Node*) malloc (sizeof(Node));
+		if (aux == NULL) {
+			printf("Error\n");
+		}
+		else {
+			aux->num = num;
+			if (l->pdi->seg == NULL) {
+				printf("Error\n");
+			}
+			else {
+				aux->num = num;
+				aux->seg = l->pdi->seg;
+				aux->ant = l->pdi;
+				l->pdi->seg->ant = aux;
+				l->pdi->seg = aux;
+			}
+		}
+	}
 }
 
-void LLISTABIDORD_insereixdavantordenat(LlistaBID *l, int num){
+void LLISTABIDORD_insereixDavantOrdenat(LlistaBID *l, int num) {
 	int trobat = 0;
 	int i = 0;
 
-	if(l->cap->seg == l->ult){
+	if (l->cap->seg == l->ult) {
 		trobat = 1;
 	}
 
@@ -157,13 +173,14 @@ void LLISTABIDORD_insereixdavantordenat(LlistaBID *l, int num){
 	if (trobat == 0){
 		l->pdi = l->pdi->seg;
 	}
-  	LLISTABIDORD_insereixdavant(l, num);
+  	LLISTABIDORD_insereixDavant(l, num);
 }
 
-void LLISTABIDORD_insereixdarreraordenat(LlistaBID *l, int num) {
+void LLISTABIDORD_insereixDarreraOrdenat(LlistaBID *l, int num) {
 	int trobat = 0;
 	int i = 0;
-  	if (l->cap-> seg == l->ult){
+	
+  	if (l->cap-> seg == l->ult) {
   		trobat = 1;
   	}
 
@@ -172,7 +189,7 @@ void LLISTABIDORD_insereixdarreraordenat(LlistaBID *l, int num) {
      		l->pdi = l->pdi->seg;
 			i++;
     	}
-    	else{
+		else {
       		trobat = 1;
 			if (i != 0){
 	  			l->pdi = l->pdi->ant;
@@ -182,6 +199,5 @@ void LLISTABIDORD_insereixdarreraordenat(LlistaBID *l, int num) {
 	if (trobat == 0){
 		l->pdi = l->pdi->ant;
 	}
-  
-	LLISTABIDORD_insereixdarrera(l, num);
+	LLISTABIDORD_insereixDarrera(l, num);
 }
