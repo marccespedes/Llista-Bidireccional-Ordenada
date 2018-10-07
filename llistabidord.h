4c63724db0e750b1957f985cb8f151fb60d1baed
Marc Céspedes <marc.cespedes@students.salle.url.edu>
2018-10-07 19:57:43 +0200

    style:Addapted to la Salle guia d'estils
    Addapted with all the charateristics of la Salle

--- llistabidord.h
+++ llistabidord.h
@@ -5,31 +5,31 @@
 
 //Definim els structs de node i els punters
 typedef struct n {
-  int num;
-  struct n *ant;
-  struct n *seg;
+	int num;
+	struct n *ant;
+	struct n *seg;
 }Node;
 
 typedef struct {
-  Node *cap;
-  Node *ult;
-  Node *pdi;
+	Node *cap;
+	Node *ult;
+	Node *pdi;
 }LlistaBID;
 
 //Capçalera de les funcions
 LlistaBID LLISTABIDORD_crea();
-void LLISTABIDORD_insereixdavantordenat(LlistaBID *l, int num);
-void LLISTABIDORD_insereixdavant(LlistaBID *l, int num);
-void LLISTABIDORD_insereixdarrera(LlistaBID *l, int num);
-void LLISTABIDORD_insereixdarreraordenat(LlistaBID *l, int num);
+void LLISTABIDORD_insereixDavantOrdenat(LlistaBID *l, int num);
+void LLISTABIDORD_insereixDavant(LlistaBID *l, int num);
+void LLISTABIDORD_insereixDarrera(LlistaBID *l, int num);
+void LLISTABIDORD_insereixDarreraOrdenat(LlistaBID *l, int num);
 int LLISTABIDORD_consulta(LlistaBID l);
 void LLISTABIDORD_esborra(LlistaBID *l);
 void LLISTABIDORD_avanca(LlistaBID * l);
 void LLISTABIDORD_retrocedeix(LlistaBID *l);
 int LLISTABIDORD_fi(LlistaBID l);
 int LLISTABIDORD_inici(LlistaBID l);
-void LLISTABIDORD_vesinici(LlistaBID *l);
-void LLISTABIDORD_vesfinal(LlistaBID *l);
+void LLISTABIDORD_vesInici(LlistaBID *l);
+void LLISTABIDORD_vesFinal(LlistaBID *l);
 int LLISTABIDORD_buida(LlistaBID l);
 void LLISTABIDORD_destrueix(LlistaBID *l);
 #endif
