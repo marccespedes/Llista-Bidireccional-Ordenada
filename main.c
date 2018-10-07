931e358da55bcea6b7701c6f2dbc623b7c459585
Marc Céspedes <marc.cespedes@students.salle.url.edu>
2018-10-07 19:58:44 +0200

    style: Addapted to guia d'estils la salle
    Addapted to characteristics of la Salle guia d'estil

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "llistabidord.h"
 
 // Aquí farem el testing
-int main (){
+int main () {
 
 	LlistaBID l;
    	int	num = 3;
@@ -11,12 +11,12 @@ int main (){
 
 	//Comprova que funcioni el llista crea
 	l = LLISTABIDORD_crea();
-		if (LLISTABIDORD_buida(l)){
+	if (LLISTABIDORD_buida(l)) {
 		printf("Funcio Buida funciona\n");
-		}
+	}
 
 	//Comprova que funcioni el insereix davant, el retrocedeix, el buida, el esborra i  el consulta
-	LLISTABIDORD_insereixdavant(&l, num);
+	LLISTABIDORD_insereixDavant(&l, num);
 	if (LLISTABIDORD_buida(l)){
 		printf("Buida no funciona\n");
 	}else{
@@ -24,59 +24,59 @@ int main (){
 	}
 	LLISTABIDORD_retrocedeix(&l);
 	LLISTABIDORD_esborra(&l);
-	LLISTABIDORD_insereixdavant(&l, 4);
-	LLISTABIDORD_insereixdavant(&l, 6);
-	LLISTABIDORD_insereixdavant(&l, 7);
-	LLISTABIDORD_insereixdavant(&l, 9);
-	LLISTABIDORD_insereixdavant(&l, 1);
+	LLISTABIDORD_insereixDavant(&l, 4);
+	LLISTABIDORD_insereixDavant(&l, 6);
+	LLISTABIDORD_insereixDavant(&l, 7);
+	LLISTABIDORD_insereixDavant(&l, 9);
+	LLISTABIDORD_insereixDavant(&l, 1);
 	LLISTABIDORD_retrocedeix(&l);
 	a = LLISTABIDORD_consulta(l);
 	printf("w%dw\n", a);
 	
 	// Comprova que  funcioni el inici, ves inici, insereix darrera i avanca.
-	LLISTABIDORD_vesinici(&l);
+	LLISTABIDORD_vesInici(&l);
 	LLISTABIDORD_retrocedeix(&l);	
 	
-	if (LLISTABIDORD_inici(l)){
+	if (LLISTABIDORD_inici(l)) {
 		LLISTABIDORD_avanca(&l);
-		LLISTABIDORD_insereixdarrera(&l, 23);
+		LLISTABIDORD_insereixDarrera(&l, 23);
 		LLISTABIDORD_avanca(&l);
 		a = LLISTABIDORD_consulta(l);
 		printf("@%d@\n", a);
 	}
 	
 	//Comprova que funcioni el ves final i fi
-	
-	LLISTABIDORD_vesfinal(&l);
+	LLISTABIDORD_vesFinal(&l);
 	LLISTABIDORD_avanca(&l);
 
-	if (LLISTABIDORD_fi(l)){
+	if (LLISTABIDORD_fi(l)) {
 		LLISTABIDORD_retrocedeix(&l);	
 		a = LLISTABIDORD_consulta(l);
 		printf("|%d|\n", a);
 	}
 
 	//Comprova que funcioni insereix davant ordenat i insereix darrera ordenat
-	LLISTABIDORD_insereixdavant(&l, 3);
-	LLISTABIDORD_insereixdavant(&l, 5);
-	LLISTABIDORD_insereixdavant(&l, 8);
-	LLISTABIDORD_insereixdavant(&l, 12);
-	LLISTABIDORD_insereixdavant(&l, 15);
-	LLISTABIDORD_vesinici(&l);
+	LLISTABIDORD_insereixDavant(&l, 3);
+	LLISTABIDORD_insereixDavant(&l, 5);
+	LLISTABIDORD_insereixDavant(&l, 8);
+	LLISTABIDORD_insereixDavant(&l, 12);
+	LLISTABIDORD_insereixDavant(&l, 15);
+	LLISTABIDORD_vesInici(&l);
 	LLISTABIDORD_avanca(&l);
 	LLISTABIDORD_avanca(&l);
-	LLISTABIDORD_insereixdavantordenat(&l, 4);
-	LLISTABIDORD_insereixdarreraordenat(&l, 13);
-	LLISTABIDORD_vesinici(&l);
-	while (l.pdi != l.ult){
-	a = LLISTABIDORD_consulta(l);
-	printf("-%d-\n", a);
-	l.pdi = l.pdi->seg;
+	LLISTABIDORD_insereixDavantOrdenat(&l, 4);
+	LLISTABIDORD_insereixDarreraOrdenat(&l, 13);
+	LLISTABIDORD_vesInici(&l);
+	
+	while (l.pdi != l.ult) {
+		a = LLISTABIDORD_consulta(l);
+		printf("-%d-\n", a);
+		l.pdi = l.pdi->seg;
 	}
 
 	// Comprova que funcioni el destrueix
 	LLISTABIDORD_destrueix(&l);
-	if (l.ult == NULL){
+	if (l.ult == NULL) {
 		printf("S'ha borrat correctment la llista\n");
 	}
 	return 0;
